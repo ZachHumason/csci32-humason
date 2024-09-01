e5b78f5934d462aa0992a41439027abdad0f0224
ZachHumason <zhumason001@student.butte.edu>
2024-08-31 17:26:29 -0700

    add next.js to project

---
+++ csci32-nextjs-humason/src/app/page.tsx
@@ -0,0 +1,9 @@
+import Image from 'next/image'
+
+export default function Home() {
+  return (
+    <a className="flex h-screen items-center justify-center">
+      <button className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-8 px-16 rounded-full">The Button</button>
+    </a>
+  )
+}
