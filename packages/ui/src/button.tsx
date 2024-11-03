807583e2a818cec873508ee2cd9707e112fb5156
ZachHumason <zhumason001@student.butte.edu>
2024-11-03 11:25:29 -0800

    Checkpoint 1

--- packages/ui/src/button.tsx
+++ packages/ui/src/button.tsx
@@ -2,14 +2,15 @@
 
 import { ReactNode } from 'react'
 import { getSizeStyles, Size } from './size'
-import { getVariantBackgroundStyles, getVariantOutlineStyles, Variant } from './variant'
-import { getCommonStyles } from './tokens'
+import { getVariantBackgroundStyles, getVariantButtonTextStyles, getVariantOutlineStyles, Variant } from './variant'
+import { getCommonButtonStyles } from './tokens'
 
 interface ButtonProps {
   children: ReactNode
   className?: string
   href?: string
   onClick?: () => void
+  type?: 'button' | 'submit' | 'reset'
   size?: Size
   variant?: Variant
 }
@@ -18,22 +19,24 @@ export const Button = ({
   children,
   className,
   href,
+  type = 'button',
   onClick,
   size = Size.MEDIUM,
   variant = Variant.PRIMARY,
 }: ButtonProps) => {
   const sizeCssClasses = getSizeStyles(size)
+  const variantButtonTextCssClasses = getVariantButtonTextStyles(variant)
   const variantBackgroundCssClasses = getVariantBackgroundStyles(variant)
   const variantOutlineCssClasses = getVariantOutlineStyles(variant)
-  const commonCssClasses = getCommonStyles()
+  const commonCssClasses = getCommonButtonStyles()
 
-  const completedCssClasses = `${sizeCssClasses} ${variantBackgroundCssClasses} ${variantOutlineCssClasses} ${commonCssClasses} ${className}`
+  const completedCssClasses = `${sizeCssClasses} ${variantBackgroundCssClasses}  ${variantOutlineCssClasses} ${commonCssClasses} ${variantButtonTextCssClasses} ${className}`
   return href ? (
     <a href={href} className={completedCssClasses}>
       {children}
     </a>
   ) : (
-    <button className={completedCssClasses} onClick={onClick}>
+    <button type={type} className={completedCssClasses} onClick={onClick}>
       {children}
     </button>
   )
--- packages/ui/src/variant.ts
+++ packages/ui/src/variant.ts
@@ -2,6 +2,8 @@ export enum Variant {
   PRIMARY,
   SECONDARY,
   TERTIARY,
+  ERROR,
+  ALERT,
 }
 
 export function getVariantBackgroundStyles(variant: Variant) {
@@ -12,6 +14,10 @@ export function getVariantBackgroundStyles(variant: Variant) {
       return 'bg-fuchsia-600 hover:bg-fuchsia-700 active:bg-fuchsia-800 text-white'
     case Variant.TERTIARY:
       return 'bg-amber-600 hover:bg-amber-700 active:bg-amber-800 text-white'
+    case Variant.ERROR:
+      return 'bg-red-600 hover:bg-red-700 active:bg-red-800'
+    case Variant.ALERT:
+      return 'bg-amber-600 hover:bg-amber-700 active:bg-amber-800'
   }
 }
 
@@ -23,6 +29,10 @@ export function getVariantOutlineStyles(variant: Variant) {
       return 'outline-fuchsia-600'
     case Variant.TERTIARY:
       return 'outline-amber-600'
+    case Variant.ERROR:
+      return 'bg-red-600 hover:bg-red-700 active:bg-red-800'
+    case Variant.ALERT:
+      return 'bg-amber-600 hover:bg-amber-700 active:bg-amber-800'
   }
 }
 
@@ -33,6 +43,10 @@ export function getVariantBorderStyles(variant: Variant) {
     case Variant.SECONDARY:
       return 'border-2 border-fuchsia-600'
     case Variant.TERTIARY:
+      return 'border-2 border-amber-600'
+    case Variant.ERROR:
+      return 'border-2 border-red-600'
+    case Variant.ALERT:
       return 'border-2 border-amber-600'
   }
 }
@@ -45,5 +59,24 @@ export function getVariantInputTextStyles(variant: Variant) {
       return 'text-black'
     case Variant.TERTIARY:
       return 'text-black'
+    case Variant.ERROR:
+      return 'text-black'
+    case Variant.ALERT:
+      return 'text-black'
+  }
+}
+
+export function getVariantButtonTextStyles(variant: Variant) {
+  switch (variant) {
+    case Variant.PRIMARY:
+      return 'text-white'
+    case Variant.SECONDARY:
+      return 'text-white'
+    case Variant.TERTIARY:
+      return 'text-white'
+    case Variant.ERROR:
+      return 'text-white'
+    case Variant.ALERT:
+      return 'text-white'
   }
 }
